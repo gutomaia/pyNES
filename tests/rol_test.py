25d739dd908d8633eba3098974c31cf1e409190d
Guto Maia <guto@guto.net>
2012-05-17 13:18:42 -0300

    added rol ror instruction

---
+++ tests/rol_test.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pynes.compiler import lexical, syntax, semantic
+class RolTest(unittest.TestCase):
+
+    def test_rol_imm(self):
+        tokens = lexical('ROL #10')
+        self.assertEquals(2 , len(tokens))
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_NUMBER', tokens[1]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x2a, 0x10])
+
+    def test_rol_zp(self):
+        tokens = lexical('ROL $00')
+        self.assertEquals(2 , len(tokens))
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ZEROPAGE', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x26, 0x00])
+
+    def test_rol_zpx(self):
+        tokens = lexical('ROL $10,X')
+        self.assertEquals(4 , len(tokens))
+        token = tokens[0]
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
+        self.assertEquals('T_REGISTER', tokens[3]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ZEROPAGE_X', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x36, 0x10])
+
+    def test_rol_abs(self):
+        tokens = lexical('ROL $1234')
+        self.assertEquals(2 , len(tokens))
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        self.assertEquals('$1234', tokens[1]['value'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ABSOLUTE', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x2e, 0x34, 0x12])
+
+
+    def test_rol_absx(self):
+        tokens = lexical('ROL $1234,X')
+        self.assertEquals(4 , len(tokens))
+        token = tokens[0]
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        self.assertEquals('$1234', tokens[1]['value'])
+        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
+        self.assertEquals('T_REGISTER', tokens[3]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ABSOLUTE_X', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x3e, 0x34, 0x12])
---
+++ tests/ror_test.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pynes.compiler import lexical, syntax, semantic
+class RorTest(unittest.TestCase):
+
+    def test_ror_imm(self):
+        tokens = lexical('ROR #10')
+        self.assertEquals(2 , len(tokens))
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_NUMBER', tokens[1]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x6a, 0x10])
+
+    def test_ror_zp(self):
+        tokens = lexical('ROR $00')
+        self.assertEquals(2 , len(tokens))
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ZEROPAGE', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x66, 0x00])
+
+    def test_ror_zpx(self):
+        tokens = lexical('ROR $10,X')
+        self.assertEquals(4 , len(tokens))
+        token = tokens[0]
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
+        self.assertEquals('T_REGISTER', tokens[3]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ZEROPAGE_X', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x76, 0x10])
+
+    def test_ror_abs(self):
+        tokens = lexical('ROR $1234')
+        self.assertEquals(2 , len(tokens))
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        self.assertEquals('$1234', tokens[1]['value'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ABSOLUTE', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x6e, 0x34, 0x12])
+
+
+    def test_ror_absx(self):
+        tokens = lexical('ROR $1234,X')
+        self.assertEquals(4 , len(tokens))
+        token = tokens[0]
+        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
+        self.assertEquals('T_ADDRESS', tokens[1]['type'])
+        self.assertEquals('$1234', tokens[1]['value'])
+        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
+        self.assertEquals('T_REGISTER', tokens[3]['type'])
+        ast = syntax(tokens)
+        self.assertEquals(1 , len(ast))
+        self.assertEquals('S_ABSOLUTE_X', ast[0]['type'])
+        code = semantic(ast)
+        self.assertEquals(code, [0x7e, 0x34, 0x12])
