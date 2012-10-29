54ce9765c5e4db8cda8b346012a35e9a5f45d237
Guto Maia <guto@guto.net>
2012-10-29 21:28:49 -0200

    added support for nametables

---
+++ pynes/nametable.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+def load_nametable(nt_file):
+    f = open('fixtures/nesasm/scrolling/SMBlevel.bin')
+    nt_content = f.read()
+    nt_bin = []
+    for nt in nt_content:
+        nt_bin.append(ord(nt))
+    return nt_bin
+
+def get_nametable(index, nt):
+    tile_index = index * 1024
+    nametable = []
+    for y in range(32):
+        line = []
+        for x in range(32):
+            dx = tile_index / 32
+            dy = tile_index % 32
+            spr_index = nt[tile_index]
+            line.append(spr_index)
+            tile_index += 1
+        nametable.append(line)
+    return nametable
+
+def length(nt):
+    return len(nt) / 1024
---
+++ pynes/tests/nametable_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pynes import nametable
+
+class NametableTest(unittest.TestCase):
+
+    def setUp(self):
+        self.nt = nametable.load_nametable('fixtures/nesasm/scrolling/SMBlevel.bin')
+        self.assertIsNotNone(self.nt)
+
+    def test_length_nametable(self):
+        length = nametable.length(self.nt)
+        self.assertEquals(4, length)
+
+    def test_get_nametable(self):
+        length = nametable.get_nametable(1, self.nt)
