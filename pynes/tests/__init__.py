c580e4059287005083650e0c6b56b0019da05718
Guto Maia <guto@guto.net>
2012-11-11 17:05:35 -0200

    refactoring on the movingsprite_test

--- pynes/tests/__init__.py
+++ pynes/tests/__init__.py
@@ -50,3 +50,51 @@ class ComposerTestCase(TestCase):
         self.cart = compose(code)
         self.asm = self.cart.to_asm()
         return WhatElse(self)
+
+class HexTestCase(TestCase):
+
+    def __init__(self, testname):
+        TestCase.__init__(self, testname)
+
+    def assertHexEquals(self, expected, actual):
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        try:
+            self.assertEquals(expected, actual)
+        except:
+            line = 0
+            cursor = 0
+            lines = []
+            out = ''
+            while (cursor < len(expected) or cursor < len(actual)):
+                for a in range(16):
+                    if cursor < len(expected) and cursor < len(actual):
+                        if expected[cursor] != actual[cursor]:
+                            lines.append(line)
+                    cursor += 1
+                line += 1
+            exp = ''
+            act = ''
+            for line in lines:
+                exp = 'Expected: %04x: ' % (line)
+                act = 'Actual  : %04x: ' % (line)
+                for a in range(16):
+                    cursor = (line * 16)+ a
+                    if cursor < len(expected) and cursor < len(actual):
+                            if expected[cursor] != actual[cursor]:
+                                exp += '%s%02x%s' % (FAIL, ord(expected[cursor]), ENDC)
+                                act += '%s%02x%s' % (FAIL, ord(actual[cursor]), ENDC)
+                            else:
+                                exp += '%02x' % ord(expected[cursor])
+                                act += '%02x' % ord(actual[cursor])
+                    if ((a+1) % 2) == 0:
+                        exp += ' '
+                        act += ' '
+                out += '%s- %d \n' % (exp, line + 1)
+                out += '%s- %d \n' % (act, line + 1)
+            print out
+            raise AssertionError('Hex are not equal')
--- pynes/tests/movingsprite_test.py
+++ pynes/tests/movingsprite_test.py
@@ -1,136 +1,93 @@
 # -*- coding: utf-8 -*-
 
-import unittest
-
 import pynes
+from pynes.tests import HexTestCase
 from pynes.compiler import lexical, syntax, semantic
 from pynes.cartridge import Cartridge
 
 #TODO: from pynes.asm import get_var
 
-class MovingSpriteTest(unittest.TestCase):
+class MovingSpriteTest(HexTestCase):
 
-    def assertHexEquals(self, expected, actual):
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        try:
-            self.assertEquals(expected, actual)
-        except:
-            line = 0
-            cursor = 0
-            lines = []
-            out = ''
-            while (cursor < len(expected) or cursor < len(actual)):
-                for a in range(16):
-                    if cursor < len(expected) and cursor < len(actual):
-                        if expected[cursor] != actual[cursor]:
-                            lines.append(line)
-                    cursor += 1
-                line += 1
-            exp = ''
-            act = ''
-            for line in lines:
-                exp = 'Expected: %04x: ' % (line)
-                act = 'Actual  : %04x: ' % (line)
-                for a in range(16):
-                    cursor = (line * 16)+ a
-                    if cursor < len(expected) and cursor < len(actual):
-                            if expected[cursor] != actual[cursor]:
-                                exp += '%s%02x%s' % (FAIL, ord(expected[cursor]), ENDC)
-                                act += '%s%02x%s' % (FAIL, ord(actual[cursor]), ENDC)
-                            else:
-                                exp += '%02x' % ord(expected[cursor])
-                                act += '%02x' % ord(actual[cursor])
-                    if ((a+1) % 2) == 0:
-                        exp += ' '
-                        act += ' '
-                out += '%s- %d \n' % (exp, line + 1)
-                out += '%s- %d \n' % (act, line + 1)
-                a
-            print out
-
-    def test_asm_compiler(self):
+    def __init__(self, testname):
+        HexTestCase.__init__(self, testname)
         f = open ('fixtures/movingsprite/movingsprite.asm')
         code = f.read()
         f.close()
         tokens = lexical(code)
-        ast = syntax(tokens)
-        #self.assertEquals(61, len(ast))
+        self.ast = syntax(tokens)
 
-        #.inesprg 1
-        self.assertEquals('S_DIRECTIVE', ast[0]['type'])
-        self.assertEquals('T_DIRECTIVE', ast[0]['children'][0]['type'])
-        self.assertEquals('.inesprg', ast[0]['children'][0]['value'])
-        self.assertEquals(5, ast[0]['children'][0]['line']);
-        self.assertEquals(3, ast[0]['children'][0]['column']);
+    def test_inesprg_1(self):
+        self.assertEquals('S_DIRECTIVE', self.ast[0]['type'])
+        self.assertEquals('T_DIRECTIVE', self.ast[0]['children'][0]['type'])
+        self.assertEquals('.inesprg', self.ast[0]['children'][0]['value'])
+        self.assertEquals(5, self.ast[0]['children'][0]['line']);
+        self.assertEquals(3, self.ast[0]['children'][0]['column']);
 
-        #.ineschr 1
-        self.assertEquals('S_DIRECTIVE', ast[1]['type'])
-        self.assertEquals('T_DIRECTIVE', ast[1]['children'][0]['type'])
-        self.assertEquals('.ineschr', ast[1]['children'][0]['value'])
-        self.assertEquals(6, ast[1]['children'][0]['line']);
-        self.assertEquals(3, ast[1]['children'][0]['column']);
+    def test_ineschr_1(self):
+        self.assertEquals('S_DIRECTIVE', self.ast[1]['type'])
+        self.assertEquals('T_DIRECTIVE', self.ast[1]['children'][0]['type'])
+        self.assertEquals('.ineschr', self.ast[1]['children'][0]['value'])
+        self.assertEquals(6, self.ast[1]['children'][0]['line']);
+        self.assertEquals(3, self.ast[1]['children'][0]['column']);
 
-        #.inesmap 0
-        self.assertEquals('S_DIRECTIVE', ast[2]['type'])
-        self.assertEquals('T_DIRECTIVE', ast[2]['children'][0]['type'])
-        self.assertEquals('.inesmap', ast[2]['children'][0]['value'])
-        self.assertEquals(7, ast[2]['children'][0]['line']);
-        self.assertEquals(3, ast[2]['children'][0]['column']);
+    def test_inesmap_0(self):
+        self.assertEquals('S_DIRECTIVE', self.ast[2]['type'])
+        self.assertEquals('T_DIRECTIVE', self.ast[2]['children'][0]['type'])
+        self.assertEquals('.inesmap', self.ast[2]['children'][0]['value'])
+        self.assertEquals(7, self.ast[2]['children'][0]['line']);
+        self.assertEquals(3, self.ast[2]['children'][0]['column']);
 
-        #.inesmir 1
-        self.assertEquals('S_DIRECTIVE', ast[3]['type'])
-        self.assertEquals('T_DIRECTIVE', ast[3]['children'][0]['type'])
-        self.assertEquals('.inesmir', ast[3]['children'][0]['value'])
-        self.assertEquals(8, ast[3]['children'][0]['line']);
-        self.assertEquals(3, ast[3]['children'][0]['column']);
+    def test_inesmir_1(self):
+        self.assertEquals('S_DIRECTIVE', self.ast[3]['type'])
+        self.assertEquals('T_DIRECTIVE', self.ast[3]['children'][0]['type'])
+        self.assertEquals('.inesmir', self.ast[3]['children'][0]['value'])
+        self.assertEquals(8, self.ast[3]['children'][0]['line']);
+        self.assertEquals(3, self.ast[3]['children'][0]['column']);
 
-        #.bank 0
-        self.assertEquals('S_DIRECTIVE', ast[4]['type'])
-        self.assertEquals('T_DIRECTIVE', ast[4]['children'][0]['type'])
-        self.assertEquals('.bank', ast[4]['children'][0]['value'])
-        self.assertEquals(11, ast[4]['children'][0]['line']);
-        self.assertEquals(3, ast[4]['children'][0]['column']);
+    def test_bank_0(self):
+        self.assertEquals('S_DIRECTIVE', self.ast[4]['type'])
+        self.assertEquals('T_DIRECTIVE', self.ast[4]['children'][0]['type'])
+        self.assertEquals('.bank', self.ast[4]['children'][0]['value'])
+        self.assertEquals(11, self.ast[4]['children'][0]['line']);
+        self.assertEquals(3, self.ast[4]['children'][0]['column']);
 
-        #.org $C000
-        self.assertEquals('S_DIRECTIVE', ast[5]['type'])
-        self.assertEquals('T_DIRECTIVE', ast[5]['children'][0]['type'])
-        self.assertEquals('.org', ast[5]['children'][0]['value'])
-        self.assertEquals(12, ast[5]['children'][0]['line']);
-        self.assertEquals(3, ast[5]['children'][0]['column']);
+    def test_org_c0000(self):
+        self.assertEquals('S_DIRECTIVE', self.ast[5]['type'])
+        self.assertEquals('T_DIRECTIVE', self.ast[5]['children'][0]['type'])
+        self.assertEquals('.org', self.ast[5]['children'][0]['value'])
+        self.assertEquals(12, self.ast[5]['children'][0]['line']);
+        self.assertEquals(3, self.ast[5]['children'][0]['column']);
 
-        # WAITVBLANK: BIT $2002
-        self.assertEquals('S_ABSOLUTE', ast[6]['type'])
-        self.assertEquals(['WAITVBLANK'], ast[6]['labels'])
-        self.assertEquals('T_INSTRUCTION', ast[6]['children'][0]['type'])
-        self.assertEquals('BIT', ast[6]['children'][0]['value'])
-        self.assertEquals(15, ast[6]['children'][0]['line']);
-        self.assertEquals(3, ast[6]['children'][0]['column']);
+    def test_waitvblank_bit_2002(self):
+        self.assertEquals('S_ABSOLUTE', self.ast[6]['type'])
+        self.assertEquals(['WAITVBLANK'], self.ast[6]['labels'])
+        self.assertEquals('T_INSTRUCTION', self.ast[6]['children'][0]['type'])
+        self.assertEquals('BIT', self.ast[6]['children'][0]['value'])
+        self.assertEquals(15, self.ast[6]['children'][0]['line']);
+        self.assertEquals(3, self.ast[6]['children'][0]['column']);
 
-        # BPL WAITVBLANK
-        self.assertEquals('S_RELATIVE', ast[7]['type'])
-        self.assertFalse('labels' in ast[7])
-        self.assertEquals('T_INSTRUCTION', ast[7]['children'][0]['type'])
-        self.assertEquals('BPL', ast[7]['children'][0]['value'])
-        self.assertEquals(16, ast[7]['children'][0]['line']);
-        self.assertEquals(3, ast[7]['children'][0]['column']);
+    def test_bpl_waitvblank(self):
+        self.assertEquals('S_RELATIVE', self.ast[7]['type'])
+        self.assertFalse('labels' in self.ast[7])
+        self.assertEquals('T_INSTRUCTION', self.ast[7]['children'][0]['type'])
+        self.assertEquals('BPL', self.ast[7]['children'][0]['value'])
+        self.assertEquals(16, self.ast[7]['children'][0]['line']);
+        self.assertEquals(3, self.ast[7]['children'][0]['column']);
 
-        # RTS
-        self.assertEquals('S_IMPLIED', ast[8]['type'])
-        self.assertFalse('labels' in ast[8])
-        self.assertEquals('T_INSTRUCTION', ast[8]['children'][0]['type'])
-        self.assertEquals('RTS', ast[8]['children'][0]['value'])
-        self.assertEquals(17, ast[8]['children'][0]['line']);
-        self.assertEquals(3, ast[8]['children'][0]['column']);
+    def test_rts(self):
+        self.assertEquals('S_IMPLIED', self.ast[8]['type'])
+        self.assertFalse('labels' in self.ast[8])
+        self.assertEquals('T_INSTRUCTION', self.ast[8]['children'][0]['type'])
+        self.assertEquals('RTS', self.ast[8]['children'][0]['value'])
+        self.assertEquals(17, self.ast[8]['children'][0]['line']);
+        self.assertEquals(3, self.ast[8]['children'][0]['column']);
 
+    def test_asm_compiler(self):
         cart = Cartridge()
         cart.path = 'fixtures/movingsprite/'
 
-        opcodes = semantic(ast, True, cart=cart)
+        opcodes = semantic(self.ast, True, cart=cart)
         #self.assertEquals(1, get_var('inesprg'))
         #self.assertEquals(1, get_var('ineschr'))
         #self.assertEquals(0, get_var('inesmap'))
@@ -141,20 +98,4 @@ class MovingSpriteTest(unittest.TestCase):
         content = f.read()
         f.close()
         self.assertHexEquals(content,bin)
-        self.assertEquals(content, bin)
-
-    def test_fragment(self):
-        fragment = '''
-          LDA #$00
-          STA $2003
-          LDA #$02
-          STA $4014
-          '''
-        tokens = lexical(fragment)
-        ast = syntax(tokens)
-        opcodes = semantic(ast)
-        bin = ''.join([chr(opcode) for opcode in opcodes])
-        f = open('fixtures/nesasm/scrolling/scrolling5.nes', 'rb')
-        content = f.read()
-        f.close()
-        self.assertTrue(bin in content)
+        self.assertEquals(content, bin) #TODO test to remove this
