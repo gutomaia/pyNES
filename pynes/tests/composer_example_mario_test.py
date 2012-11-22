15fcfe28994563b9f52361e740076f6702c13e13
Guto Maia <guto@guto.net>
2012-11-22 16:47:26 -0200

    added a nes composer example test

---
+++ pynes/tests/composer_example_mario_test.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pynes.tests import ComposerTestCase
+
+class ComposerMarioTest(ComposerTestCase):
+
+    def test_mario(self):
+        self.path = 'fixtures/nesasm/scrolling/'
+
+        f = open('pynes/examples/mario.py')
+        code = f.read()
+        f.close()
+        (
+            self.assert_asm_from(code)
+                .has('.bank 0')
+                .and_then('WAITVBLANK:')
+                .and_then('RESET:')
+                .and_then('JSR WAITVBLANK')
+                .and_then('CLEARMEM:')
+                .and_then('JSR WAITVBLANK')
+                .and_then('LoadPalettes:')
+                .and_then('NMI:')
+                .and_then('.bank 1')
+                .and_then('palette:')
+                .and_then('tinymario:')
+                .and_then('mario:')
+        )
