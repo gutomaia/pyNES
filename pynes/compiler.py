d71148d827d1cd118751b860148f56f104dea183
Guto Maia <guto@guto.net>
2013-06-24 10:02:55 -0300

    more pep8 fixes

--- pynes/compiler.py
+++ pynes/compiler.py
@@ -14,10 +14,15 @@ from pynes.cartridge import Cartridge
 import pynes
 
 asm65_tokens = [
-    dict(type='T_INSTRUCTION', regex=r'^(ADC|AND|ASL|BCC|BCS|BEQ|BIT|BMI|BNE|BPL|BRK|BVC|BVS|CLC|CLD|CLI|CLV|CMP|CPX|CPY|DEC|DEX|DEY|EOR|INC|INX|INY|JMP|JSR|LDA|LDX|LDY|LSR|NOP|ORA|PHA|PHP|PLA|PLP|ROL|ROR|RTI|RTS|SBC|SEC|SED|SEI|STA|STX|STY|TAX|TAY|TSX|TXA|TXS|TYA)', store=True),
+    dict(type='T_INSTRUCTION',
+         regex=(r'^(ADC|AND|ASL|BCC|BCS|BEQ|BIT|BMI|BNE|BPL|BRK|BVC|BVS|CLC|'
+                'CLD|CLI|CLV|CMP|CPX|CPY|DEC|DEX|DEY|EOR|INC|INX|INY|JMP|JSR|'
+                'LDA|LDX|LDY|LSR|NOP|ORA|PHA|PHP|PLA|PLP|ROL|ROR|RTI|RTS|SBC|'
+                'SEC|SED|SEI|STA|STX|STY|TAX|TAY|TSX|TXA|TXS|TYA)'),
+         store=True),
     dict(type='T_ADDRESS', regex=r'\$([\dA-F]{2,4})', store=True),
     dict(type='T_HEX_NUMBER', regex=r'\#\$([\dA-F]{2})', store=True),
-    dict(type='T_BINARY_NUMBER', regex=r'\#?%([01]{8})', store=True), #TODO: just to compile background.asm
+    dict(type='T_BINARY_NUMBER', regex=r'\#?%([01]{8})', store=True),
     dict(type='T_DECIMAL_NUMBER', regex=r'\#(\d{1,3})', store=True),
     dict(type='T_LABEL', regex=r'^([a-zA-Z]{2}[a-zA-Z\d]*)\:', store=True),
     dict(type='T_MARKER', regex=r'^[a-zA-Z]{2}[a-zA-Z\d]*', store=True),
@@ -37,107 +42,136 @@ asm65_tokens = [
     dict(type='T_COMMENT', regex=r'^;[^\n]*', store=False)
 ]
 
+
 def look_ahead(tokens, index, type, value = None):
     if index > len(tokens) - 1:
         return 0
     token = tokens[index]
     if token['type'] == type:
-        if value == None or token['value'].upper() == value.upper():
+        if value is None or token['value'].upper() == value.upper():
             return 1
     return 0
+
 
 def t_endline(tokens, index):
     return look_ahead(tokens, index, 'T_ENDLINE', '\n')
 
+
 def t_modifier(tokens, index):
     return look_ahead(tokens, index, 'T_MODIFIER')
+
 
 def t_directive(tokens, index):
     return look_ahead(tokens, index, 'T_DIRECTIVE')
 
+
 def t_directive_argument(tokens, index):
-    return OR([t_list, t_address, t_marker, t_address, t_decimal_argument, t_string], tokens, index)
+    return OR([t_list, t_address, t_marker, t_address,
+               t_decimal_argument, t_string], tokens, index)
+
 
 def t_decimal_argument(tokens, index):
     return look_ahead(tokens, index, 'T_DECIMAL_ARGUMENT')
 
+
 def t_relative(tokens, index):
-    if (look_ahead(tokens, index, 'T_INSTRUCTION') and 
-        tokens[index]['value'] in [
-            'BCC', 'BCS', 'BEQ', 'BNE',
-            'BMI', 'BPL', 'BVC', 'BVS'
-        ]):
+    if (look_ahead(tokens, index, 'T_INSTRUCTION')
+            and tokens[index]['value'] in ['BCC', 'BCS', 'BEQ', 'BNE',
+                                           'BMI', 'BPL', 'BVC', 'BVS']):
         return 1
     return 0
 
-def t_instruction (tokens, index):
+
+def t_instruction(tokens, index):
     return look_ahead(tokens, index, 'T_INSTRUCTION')
 
-def t_zeropage (tokens,index):
+
+def t_zeropage(tokens, index):
     lh = look_ahead(tokens, index, 'T_ADDRESS')
     if lh and len(tokens[index]['value']) == 3:
         return 1
     return 0
 
+
 def t_label(tokens, index):
     return look_ahead(tokens, index, 'T_LABEL')
+
 
 def t_marker(tokens, index):
     return look_ahead(tokens, index, 'T_MARKER')
 
+
 def t_address(tokens, index):
     return look_ahead(tokens, index, 'T_ADDRESS')
+
 
 def t_string(tokens, index):
     return look_ahead(tokens, index, 'T_STRING')
 
+
 def t_address_or_t_marker(tokens, index):
     return OR([t_address, t_marker], tokens, index)
+
 
 def t_address_or_t_binary_number(tokens, index):
     return OR([t_address, t_binary_number], tokens, index)
 
+
 def t_hex_number(tokens, index):
     return look_ahead(tokens, index, 'T_HEX_NUMBER')
+
 
 def t_binary_number(tokens, index):
     return look_ahead(tokens, index, 'T_BINARY_NUMBER')
 
+
 def t_decimal_number(tokens, index):
     return look_ahead(tokens, index, 'T_DECIMAL_NUMBER')
+
 
 def t_number(tokens, index):
     return OR([t_hex_number, t_binary_number, t_decimal_number], tokens, index)
 
-def t_separator(tokens , index):
+
+def t_separator(tokens, index):
     return look_ahead(tokens, index, 'T_SEPARATOR')
+
 
 def t_accumulator(tokens, index):
     return look_ahead(tokens, index, 'T_ACCUMULATOR', 'A')
 
+
 def t_register_x(tokens, index):
     return look_ahead(tokens, index, 'T_REGISTER', 'X')
+
 
 def t_register_y(tokens, index):
     return look_ahead(tokens, index, 'T_REGISTER', 'Y')
 
+
 def t_open(tokens, index):
     return look_ahead(tokens, index, 'T_OPEN', '(')
+
 
 def t_close(tokens, index):
     return look_ahead(tokens, index, 'T_CLOSE', ')')
 
+
 def t_open_square_brackets(tokens, index):
     return look_ahead(tokens, index, 'T_OPEN_SQUARE_BRACKETS', '[')
+
 
 def t_close_square_brackets(tokens, index):
     return look_ahead(tokens, index, 'T_CLOSE_SQUARE_BRACKETS', ']')
 
+
 def t_nesasm_compatible_open(tokens, index):
         return OR([t_open, t_open_square_brackets], tokens, index)
 
+
 def t_nesasm_compatible_close(tokens, index):
         return OR([t_close, t_close_square_brackets], tokens, index)
+
 
 def t_list(tokens, index):
     if t_address_or_t_binary_number(tokens, index) and t_separator(tokens, index+1):
@@ -146,13 +180,14 @@ def t_list(tokens, index):
         while (islist):
             islist = islist and t_separator(tokens, index + (arg * 2) + 1)
             islist = islist and t_address_or_t_binary_number(tokens, index + (arg * 2) + 2)
-            if (t_endline(tokens, index + (arg * 2) + 3) or
-                (index + (arg * 2) + 3) == len(tokens)):
-                break;
+            if (t_endline(tokens, index + (arg * 2) + 3)
+                    or (index + (arg * 2) + 3) == len(tokens)):
+                break
             arg += 1
         if islist:
             return ((arg + 1) * 2) + 1
     return 0
+
 
 def get_list_jump(tokens, index):
     keep = True
@@ -176,7 +211,7 @@ asm65_bnf = [
     dict(type='S_DIRECTIVE', bnf=[t_directive, t_directive_argument]),
     dict(type='S_RELATIVE', bnf=[t_relative, t_address_or_t_marker]),
     dict(type='S_IMMEDIATE', bnf=[t_instruction, t_number]),
-    dict(type='S_IMMEDIATE_WITH_MODIFIER', bnf=[t_instruction, t_modifier, t_open, t_address_or_t_marker, t_close]), #nesasm hack
+    dict(type='S_IMMEDIATE_WITH_MODIFIER', bnf=[t_instruction, t_modifier, t_open, t_address_or_t_marker, t_close]),  # nesasm hack
     dict(type='S_ACCUMULATOR', bnf=[t_instruction, t_accumulator]),
     dict(type='S_ZEROPAGE_X', bnf=[t_instruction, t_zeropage, t_separator, t_register_x]),
     dict(type='S_ZEROPAGE_Y', bnf=[t_instruction, t_zeropage, t_separator, t_register_y]),
@@ -219,15 +254,15 @@ def get_value(token, labels = []):
 
 def syntax(tokens):
     ast = []
-    x = 0 # consumed
+    x = 0  # consumed
     debug = 0
     labels = []
     move = 0
     while (x < len(tokens)):
-        if t_label(tokens,x):
+        if t_label(tokens, x):
             labels.append(get_value(tokens[x]))
             x += 1
-        elif t_endline(tokens,x):
+        elif t_endline(tokens, x):
             x += 1
         else:
             for bnf in asm65_bnf:
@@ -235,15 +270,15 @@ def syntax(tokens):
                 look_ahead = 0
                 move = 0
                 for i in bnf['bnf']:
-                    move = i(tokens,x + look_ahead)
+                    move = i(tokens, x + look_ahead)
                     if not move:
-                        break;
+                        break
                     look_ahead += 1
                 if move:
                     if len(labels) > 0:
                         leaf['labels'] = labels
                         labels = []
-                    size = 0;
+                    size = 0
                     look_ahead = 0;
                     for b in bnf['bnf']:
                         size += b(tokens, x+look_ahead)
@@ -252,7 +287,7 @@ def syntax(tokens):
                     leaf['type'] = bnf['type']
                     ast.append(leaf)
                     x += size
-                    break;
+                    break
             if not move:
                 #TODO: deal with erros like on nodeNES
                 walk = 0
@@ -277,7 +312,7 @@ def get_labels(ast):
             for label in leaf['labels']:
                 labels[label] = address
         if leaf['type'] != 'S_DIRECTIVE' and leaf['type'] != 'S_RS':
-            size =  address_mode_def[leaf['type']]['size']
+            size = address_mode_def[leaf['type']]['size']
             address += size
         elif 'S_DIRECTIVE' == leaf['type'] and '.db' == leaf['children'][0]['value']:
             for i in leaf['children']:
