4ce284f0ec913173d1de4a9cfe8577abde4830e6
Ellison Leão <ellisonleao@gmail.com>
2014-04-30 01:12:15 -0300

    PEP8 fixes on tests/asl_test.py

--- pynes/tests/asl_test.py
+++ pynes/tests/asl_test.py
@@ -10,88 +10,86 @@ from pynes.compiler import lexical, syntax, semantic
 
 class AslTest(unittest.TestCase):
 
-    #TODO see the accumulator type instruction, ASL A
-
+    # TODO see the accumulator type instruction, ASL A
     def test_asl_imm(self):
         tokens = lexical('ASL #$10')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_HEX_NUMBER', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_IMMEDIATE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x0a, 0x10])
 
     def test_asl_imm_with_decimal(self):
         tokens = lexical('ASL #10')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_DECIMAL_NUMBER', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_IMMEDIATE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x0a, 0x0a])
 
     def test_asl_imm_with_binary(self):
         tokens = lexical('ASL #%00000100')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_BINARY_NUMBER', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_IMMEDIATE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x0a, 0x04])
 
     def test_asl_zp(self):
         tokens = lexical('ASL $00')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ZEROPAGE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x06, 0x00])
 
     def test_asl_zpx(self):
         tokens = lexical('ASL $10,X')
-        self.assertEquals(4 , len(tokens))
+        self.assertEquals(4, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         self.assertEquals('T_SEPARATOR', tokens[2]['type'])
         self.assertEquals('T_REGISTER', tokens[3]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ZEROPAGE_X', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x16, 0x10])
 
     def test_asl_abs(self):
         tokens = lexical('ASL $1234')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         self.assertEquals('$1234', tokens[1]['value'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ABSOLUTE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x0e, 0x34, 0x12])
 
-
     def test_asl_absx(self):
         tokens = lexical('ASL $1234,X')
-        self.assertEquals(4 , len(tokens))
+        self.assertEquals(4, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         self.assertEquals('$1234', tokens[1]['value'])
         self.assertEquals('T_SEPARATOR', tokens[2]['type'])
         self.assertEquals('T_REGISTER', tokens[3]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ABSOLUTE_X', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x1e, 0x34, 0x12])
