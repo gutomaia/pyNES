e19664f39250233cf837422a6a59576efc699c5c
Ellison Leão <ellisonleao@gmail.com>
2014-04-30 00:37:58 -0300

    PEP8 fixes on tests/rti_test.py

--- pynes/tests/rti_test.py
+++ pynes/tests/rti_test.py
@@ -4,14 +4,15 @@ import unittest
 
 from pynes.compiler import lexical, syntax, semantic
 
+
 class RtiTest(unittest.TestCase):
 
     def test_rti_sngl(self):
         tokens = lexical('RTI')
-        self.assertEquals(1 , len(tokens))
+        self.assertEquals(1, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_IMPLIED', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x40])
