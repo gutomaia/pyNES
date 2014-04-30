1364389518cee5053adda95a030f8cb4b56dcb27
Ellison Leão <ellisonleao@gmail.com>
2014-04-30 01:13:20 -0300

    PEP8 fixes on tests/and_test.py

--- pynes/tests/and_test.py
+++ pynes/tests/and_test.py
@@ -3,15 +3,15 @@
 AND, Logical AND with Accumulator Test
 
 This is a test for the logical instruction AND of
-the 6502. In the 6502 the logical AND could be 
+the 6502. In the 6502 the logical AND could be
 performed against the content of the accumulator or
 a content at a specific location.
 '''
 import unittest
 from pynes.compiler import lexical, syntax, semantic
 
-class AndTest(unittest.TestCase):
 
+class AndTest(unittest.TestCase):
 
     def test_and_imm(self):
         '''
@@ -19,11 +19,11 @@ class AndTest(unittest.TestCase):
         and the content of the Accumulator
         '''
         tokens = lexical('AND #$10')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_HEX_NUMBER', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_IMMEDIATE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x29, 0x10])
@@ -34,11 +34,11 @@ class AndTest(unittest.TestCase):
         and the content of the Accumulator
         '''
         tokens = lexical('AND #10')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_DECIMAL_NUMBER', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_IMMEDIATE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x29, 0x0a])
@@ -49,11 +49,11 @@ class AndTest(unittest.TestCase):
         and the content of the Accumulator
         '''
         tokens = lexical('AND #%00000100')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_BINARY_NUMBER', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_IMMEDIATE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x29, 0x04])
@@ -64,11 +64,11 @@ class AndTest(unittest.TestCase):
         accumulator and the content of zero page address $00
         '''
         tokens = lexical('AND $00')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ZEROPAGE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x25, 0x00])
@@ -80,60 +80,59 @@ class AndTest(unittest.TestCase):
         address calculated from $10 adding content of X
         '''
         tokens = lexical('AND $10,X')
-        self.assertEquals(4 , len(tokens))
+        self.assertEquals(4, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         self.assertEquals('T_SEPARATOR', tokens[2]['type'])
         self.assertEquals('T_REGISTER', tokens[3]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ZEROPAGE_X', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x35, 0x10])
 
     def test_and_abs(self):
         tokens = lexical('AND $1234')
-        self.assertEquals(2 , len(tokens))
+        self.assertEquals(2, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         self.assertEquals('$1234', tokens[1]['value'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ABSOLUTE', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x2d, 0x34, 0x12])
 
-
     def test_and_absx(self):
         tokens = lexical('AND $1234,X')
-        self.assertEquals(4 , len(tokens))
+        self.assertEquals(4, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         self.assertEquals('$1234', tokens[1]['value'])
         self.assertEquals('T_SEPARATOR', tokens[2]['type'])
         self.assertEquals('T_REGISTER', tokens[3]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ABSOLUTE_X', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x3d, 0x34, 0x12])
 
     def test_and_absy(self):
         tokens = lexical('AND $1234,Y')
-        self.assertEquals(4 , len(tokens))
+        self.assertEquals(4, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_ADDRESS', tokens[1]['type'])
         self.assertEquals('T_SEPARATOR', tokens[2]['type'])
         self.assertEquals('T_REGISTER', tokens[3]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_ABSOLUTE_Y', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x39, 0x34, 0x12])
 
     def test_and_indx(self):
         tokens = lexical('AND ($20,X)')
-        self.assertEquals(6 , len(tokens))
+        self.assertEquals(6, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_OPEN', tokens[1]['type'])
         self.assertEquals('T_ADDRESS', tokens[2]['type'])
@@ -142,14 +141,14 @@ class AndTest(unittest.TestCase):
         self.assertEquals('T_REGISTER', tokens[4]['type'])
         self.assertEquals('T_CLOSE', tokens[5]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_INDIRECT_X', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x21, 0x20])
 
     def test_and_indy(self):
         tokens = lexical('AND ($20),Y')
-        self.assertEquals(6 , len(tokens))
+        self.assertEquals(6, len(tokens))
         self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
         self.assertEquals('T_OPEN', tokens[1]['type'])
         self.assertEquals('T_ADDRESS', tokens[2]['type'])
@@ -157,7 +156,7 @@ class AndTest(unittest.TestCase):
         self.assertEquals('T_SEPARATOR', tokens[4]['type'])
         self.assertEquals('T_REGISTER', tokens[5]['type'])
         ast = syntax(tokens)
-        self.assertEquals(1 , len(ast))
+        self.assertEquals(1, len(ast))
         self.assertEquals('S_INDIRECT_Y', ast[0]['type'])
         code = semantic(ast)
         self.assertEquals(code, [0x31, 0x20])
