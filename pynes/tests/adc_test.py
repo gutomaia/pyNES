d73ecbc84bb6c97b7980628eec1a486ae848cfff
Guto Maia <guto@guto.net>
2014-09-07 16:59:06 -0300

    refactoring adc_test

--- pynes/tests/adc_test.py
+++ pynes/tests/adc_test.py
@@ -6,181 +6,138 @@ This is an arithmetic instruction of the 6502.
 '''
 
 import unittest
-from pynes.compiler import lexical, syntax, semantic
+from pynes.tests import MetaInstructionCase
 
 
-class AdcTest(unittest.TestCase):
+class AdcImmTest(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between decimal 16
+    and the content of the accumulator.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC #$10'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_HEX_NUMBER', '#$10')]
+    syn = ['S_IMMEDIATE']
+    code = [0x69, 0x10]
 
-    def test_adc_imm(self):
-        '''
-        Test the arithmetic operation ADC between decimal 16
-        and the content of the accumulator.
-        '''
-        tokens = list(lexical('ADC #$10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_HEX_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x69, 0x10])
 
-    def test_adc_imm_with_decimal(self):
-        '''
-        Test the arithmetic operation ADC between decimal 10
-        and the content of the accumulator.
-        '''
-        tokens = list(lexical('ADC #10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_DECIMAL_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x69, 0x0A])
+class AdcImmWithDecimalTest(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between decimal 10
+    and the content of the accumulator.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC #10'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_DECIMAL_NUMBER', '#10')]
+    syn = ['S_IMMEDIATE']
+    code = [0x69, 0x0A]
 
-    def test_adc_imm_with_binary(self):
-        '''
-        Test the arithmetic operation ADC between binary %00000100
-        (Decimal 4) and the content of the accumulator.
-        '''
-        tokens = list(lexical('ADC #%00000100'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_BINARY_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x69, 0x04])
 
-    def test_adc_zp(self):
-        '''
-        Test the arithmetic operation ADC between the content of
-        the accumulator and the content of the zero page address.
-        '''
-        tokens = list(lexical('ADC $00'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ZEROPAGE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x65, 0x00])
+class AdcImmWithBinaryTest(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between binary %00000100
+    (Decimal 4) and the content of the accumulator.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC #%00000100'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_BINARY_NUMBER', '#%00000100')]
+    syn = ['S_IMMEDIATE']
+    code = [0x69, 0x04]
 
-    def test_adc_zpx(self):
-        '''
-        Test the arithmetic operation ADC between the content of the
-        accumulator and the content of the zero page with address
-        calculated from $10 adding content of X.
-        '''
-        tokens = list(lexical('ADC $10,X'))
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
-        self.assertEquals('T_REGISTER', tokens[3]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ZEROPAGE_X', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x75, 0x10])
 
-    def test_adc_abs(self):
-        '''
-        Test the arithmetic operation ADC between the content of
-        the accumulator and the content located at address $1234.
-        '''
-        tokens = list(lexical('ADC $1234'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('$1234', tokens[1]['value'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x6d, 0x34, 0x12])
+class AdcZpTest(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between the content of
+    the accumulator and the content of the zero page address.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC $00'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_ADDRESS', '$00')]
+    syn = ['S_ZEROPAGE']
+    code = [0x65, 0x00]
 
-    def test_adc_absx(self):
-        '''
-        Test the arithmetic operation ADC between the content of the
-        accumulator and the content located at address $1234
-        adding the content of X.
-        '''
-        tokens = list(lexical('ADC $1234,X'))
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('$1234', tokens[1]['value'])
-        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
-        self.assertEquals('T_REGISTER', tokens[3]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE_X', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x7d, 0x34, 0x12])
 
-    def test_adc_absy(self):
-        '''
-        Test the arithmetic operation ADC between the content of the
-        accumulator and the content located at address $1234
-        adding the content of Y.
-        '''
-        tokens = list(lexical('ADC $1234,Y'))
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
-        self.assertEquals('T_REGISTER', tokens[3]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE_Y', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x79, 0x34, 0x12])
+class AdcZpxTest(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between the content of the
+    accumulator and the content of the zero page with address
+    calculated from $10 adding content of X.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC $10,X'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_ADDRESS', '$10'),
+           ('T_SEPARATOR', ','),('T_REGISTER','X')]
+    syn = ['S_ZEROPAGE_X']
+    code = [0x75, 0x10]
 
-    def test_adc_indx(self):
-        '''
-        Test the arithmetic ADC operation between the content of the
-        accumulator and the content located at the address
-        obtained from the address calculated from the value
-        stored in the address $20 adding the content of Y.
-        '''
-        tokens = list(lexical('ADC ($20,X)'))
-        self.assertEquals(6, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_OPEN', tokens[1]['type'])
-        self.assertEquals('T_ADDRESS', tokens[2]['type'])
-        self.assertEquals('$20', tokens[2]['value'])
-        self.assertEquals('T_SEPARATOR', tokens[3]['type'])
-        self.assertEquals('T_REGISTER', tokens[4]['type'])
-        self.assertEquals('T_CLOSE', tokens[5]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_INDIRECT_X', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x61, 0x20])
 
-    def test_adc_indy(self):
-        '''
-        Test arithmetic operation ADC between the content of the
-        accumulator and the content located at the address
-        obtained from the address calculated from the value
-        stored in the address $20 adding the content of Y.
-        '''
-        tokens = list(lexical('ADC ($20),Y'))
-        self.assertEquals(6, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_OPEN', tokens[1]['type'])
-        self.assertEquals('T_ADDRESS', tokens[2]['type'])
-        self.assertEquals('T_CLOSE', tokens[3]['type'])
-        self.assertEquals('T_SEPARATOR', tokens[4]['type'])
-        self.assertEquals('T_REGISTER', tokens[5]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_INDIRECT_Y', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0x71, 0x20])
+class AdcAbsTest(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between the content of
+    the accumulator and the content located at address $1234.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC $1234'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_ADDRESS', '$1234')]
+    syn = ['S_ABSOLUTE']
+    code = [0x6d, 0x34, 0x12]
+
+
+class AdcAbsx(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between the content of the
+    accumulator and the content located at address $1234
+    adding the content of X.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC $1234,X'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_ADDRESS', '$1234'),
+           ('T_SEPARATOR', ','), ('T_REGISTER', 'X')]
+    syn = ['S_ABSOLUTE_X']
+    code = [0x7d, 0x34, 0x12]
+
+
+class AdcAbsy(unittest.TestCase):
+    '''
+    Test the arithmetic operation ADC between the content of the
+    accumulator and the content located at address $1234
+    adding the content of Y.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC $1234,Y'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_ADDRESS', '$1234'),
+           ('T_SEPARATOR', ','), ('T_REGISTER', 'Y')]
+    syn = ['S_ABSOLUTE_Y']
+    code = [0x79, 0x34, 0x12]
+
+
+class AdcIndx(unittest.TestCase):
+    '''
+    Test the arithmetic ADC operation between the content of the
+    accumulator and the content located at the address
+    obtained from the address calculated from the value
+    stored in the address $20 adding the content of Y.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC ($20,X)'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_OPEN', '('),
+           ('T_ADDRESS', '$20'), ('T_SEPARATOR', ','),
+           ('T_REGISTER', 'X'), ('T_CLOSE', ')')]
+    syn = ['S_INDIRECT_X']
+    code = [0x61, 0x20]
+
+
+class AdcIndy(unittest.TestCase):
+    '''
+    Test arithmetic operation ADC between the content of the
+    accumulator and the content located at the address
+    obtained from the address calculated from the value
+    stored in the address $20 adding the content of Y.
+    '''
+    __metaclass__ = MetaInstructionCase
+    asm = 'ADC ($20),Y'
+    lex = [('T_INSTRUCTION', 'ADC'), ('T_OPEN', '('),
+           ('T_ADDRESS', '$20'), ('T_CLOSE', ')'),
+           ('T_SEPARATOR', ','), ('T_REGISTER', 'Y')]
+    syn = ['S_INDIRECT_Y']
+    code = [0x71, 0x20]
