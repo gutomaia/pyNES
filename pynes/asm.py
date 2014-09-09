20bf8f38f1387357db6e72dea93ec199e50898c3
Guto Maia <guto@guto.net>
2014-09-08 23:19:41 -0300

    improving the asm call

--- pynes/asm.py
+++ pynes/asm.py
@@ -2,15 +2,14 @@ from pynes.c6502 import opcodes
 from pynes.utils import Context
 import sys
 
-this = sys.modules[__name__]
+__all__ = opcodes.keys()
 
 
-class Asm(object):
-    context = Context()
+class InstructionProxy():
 
-    def __init__(self, *args, **kw):
-        if 'instruction' in kw:
-            self.i = kw['instruction']
+    def __init__(self, name):
+        self.i = name
+        self.context = Context()
 
     def __call__(self, *args, **kw):
         if self.context == None:
@@ -23,5 +22,18 @@ class Asm(object):
             self.context += '%s\n' % self.i
 
 
-for k,v in opcodes.items():
-    setattr(this, k, Asm(instruction=k))
+class Wrapper(object):
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __getattr__(self, name):
+        if name in opcodes.keys():
+            return InstructionProxy(name)
+        elif hasattr(self.wrapped, name):
+            return getattr(self.wrapped, name)
+        else:
+            raise AttributeError(name)
+
+
+sys.modules[__name__] = Wrapper(sys.modules[__name__])
