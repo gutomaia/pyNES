f485d8f86ee80cabe965203cd0773a6e46f8e637
Guto Maia <guto@guto.net>
2015-09-27 20:08:03 -0300

    bugfix: setup.py was not able to open README.rst

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 
